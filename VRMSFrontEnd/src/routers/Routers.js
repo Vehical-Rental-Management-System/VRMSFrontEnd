e332850c7d696215f017e87dad509f9bd0131d0a
Akshay <akphapale99@gmail.com>
2023-08-23 16:03:26 +0530

    Admin Front end components added

--- VRMSFrontEnd/src/routers/Routers.js
+++ VRMSFrontEnd/src/routers/Routers.js
@@ -1,65 +1,77 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "../pages/Home";
-import About from "../pages/About";
-import CarListing from "../pages/CarListing";
-import CarDetails from "../pages/CarDetails";
-import NotFound from "../pages/NotFound";
-import Contact from "../pages/Contact";
-import Login from "../pages/Login";
-import Register from "../pages/SignUp";
-import UpdateProfile from "../pages/UpdateProfile";
-import PaymentForm from "../pages/PaymentForm";
+import LoginHeader from "./components/UI/LoginHeader";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import { useDispatch, useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
-import AvailableVehicles from '../pages/AvailableVehicles';
-import ReservedVehicles from '../pages/ReservedVehicles';
-import AddVehicle from '../pages/AddVehicle';
-import ServiceLocations from "../pages/ServiceLocation";
-import AddServiceLocation from "../pages/AddServiceLocation";
-import Bookings from "../pages/Bookings";
-import BookingsWithFeedback from "../pages/BookingsWithFeedback";
-import WebsiteFeedback from "../pages/WebsiteFeedback";
-import Users from '../pages/Users';
-import SideBar from '../components/Sidebar';
-import sidebar_menu from '../constants/sidebar-menu';
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import CarListing from "./pages/CarListing";
+import CarDetails from "./pages/CarDetails";
+import NotFound from "./pages/NotFound";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Register from "./pages/SignUp";
+import UpdateProfile from "./pages/UpdateProfile";
+import PaymentForm from "./pages/PaymentForm";
+import AllVehicles from './pages/AllVehicles';
+import AvailableVehicles from './pages/AvailableVehicles';
+import ReservedVehicles from './pages/ReservedVehicles';
+import Users from './pages/Users';
+import SideBar from './components/Sidebar';
+import sidebar_menu from './constants/sidebar-menu';
+import UserProfile from "./pages/Users/UserProfile";
+import ViewProfile from "./pages/Users/ViewProfile";
+import ChangePassword from "./pages/Users/ChangePassword";
+import MyBookings from "./pages/Users/MyBookings";
 
-const Routers = () => {
-    return (<>
+function App() {
 
-        {/* <div className='dashboard-container'> */}
-        <div className='row'>
-        <div className='col-md-2'><SideBar menu={sidebar_menu} /></div>
+  
+    const location = useLocation();
+
+    console.log(location.pathname);
+
+    const role = useSelector((state) => state.auth.role)
+
+  return (
+    <>
+      
+      { location.pathname=="/login" ?<LoginHeader></LoginHeader>: location.pathname=="/register" ? <LoginHeader></LoginHeader>:role=="Admin"?<></>:<Header /> }
+      <div className='row'>
+      {role=="Admin" &&  <div className='col-md-2'><SideBar menu={sidebar_menu} /></div>}
             
-        <div className='col-md-10'>
-            {/* <div className='dashboard-body'> */}
+        
                 <Routes>
                     <Route path="/" element={<Navigate to="/home" />} />
                     <Route path="/home" element={<Home />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/cars" element={<CarListing />} />
-                    <Route path="/cars/:slug" element={<CarDetails />} />
+                    <Route path="/cars/:id" element={<CarDetails />} />
                     <Route path="/contact" element={<Contact />} />
                     <Route path="/payment" element={<PaymentForm />} />
                     <Route path="/updateprofile" element={<UpdateProfile />} />
+                    <Route path="/viewProfile" element={<ViewProfile />} />
+                    <Route path="/changePassword" element={<ChangePassword />} />
+                    <Route path="/myBooking" element={<MyBookings />} />
                     <Route path="/login" element={<Login />} />
+                    <Route path="/logout" element={<Home />} />
+                    
                     <Route path="/register" element={<Register />} />
-
+                    </Routes>
                     {/* Admin route  */}
-
+                    <div className='col-md-10'>
+                    <Routes>
                   
                         <Route path="*" element={<div></div>} />
                         <Route exact path="/" element={<div></div>} />
+                        <Route exact path="/AllVehicles" element={< AllVehicles />} />
                         <Route exact path="/AvailableVehicles" element={< AvailableVehicles />} />
                         <Route exact path="/ReservedVehicles" element={< ReservedVehicles />} />
-                        <Route exact path="/AddVehicle" element={< AddVehicle />} />
                         <Route exact path="/Users" element={< Users />} />
-                        <Route exact path="/ServiceLocations" element={< ServiceLocations />} />
-                        <Route exact path="/AddServiceLocation" element={< AddServiceLocation />} />
-                        <Route exact path="/Bookings" element={< Bookings />} />
-                        <Route exact path="/BookingsWithFeedback" element={< BookingsWithFeedback />} />
-                        <Route exact path="/WebsiteFeedback" element={< WebsiteFeedback />} />
                         <Route exact path="/locations" element={<div></div>} />
-                        <Route exact path="/profile" element={<div></div>} />
+                        <Route exact path="/profile" element={< div />} />
+                        {/* <Route exact path="/addServiceProvider" element={ <addServiceProvider /> }/> */}
                   
 
 
@@ -67,12 +79,11 @@ const Routers = () => {
                 </Routes>
             </div>
             </div>
-          {/* <div className='col'></div> */}
-            {/* </div> */}
-        {/* </div> */}
-        <ToastContainer />
+      { location.pathname=="/login" ?<></> :location.pathname=="/register" ? <></>:role=="Admin"?<></>:<Footer /> }
+    
+      <ToastContainer />
     </>
-    );
-};
+  );
+}
 
-export default Routers;
+export default App;
